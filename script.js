7882a47833a0aada96910eaa1ef8f0d3318cb920
Amir <amir.batyrggaliev@gmail.com>
2025-01-21 17:07:20 +0500

    Implement random computer choice function

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+let choices = ["rock", "paper", "scissors"];
 
+function getComputerChoice(choices) {
+  let getRandomNumber = Math.floor(Math.random() * 3);
+  return choices[getRandomNumber];
+}
+
+console.log(getComputerChoice(choices));
