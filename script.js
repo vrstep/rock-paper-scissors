6c2462d2d43a9060727462ecde9af5129109886d
Amir <amir.batyrggaliev@gmail.com>
2025-01-22 21:11:49 +0500

    Implement getting user input

--- script.js
+++ script.js
@@ -5,4 +5,15 @@ function getComputerChoice(choices) {
   return choices[getRandomNumber];
 }
 
+function getHumanChoice() {
+  let getUserInput = prompt("What's your move?", "").toLowerCase();
+
+  while (!choices.includes(getUserInput)) {
+    console.log("Invalid input, sir!");
+    getUserInput = prompt("What's your move?", "");
+  }
+  return getUserInput;
+}
+
 console.log(getComputerChoice(choices));
+console.log(getHumanChoice());
